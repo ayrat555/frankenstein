aab99da4704308001354985768499d8a8c985f3a
Ayrat Badykov <ayratin555@gmail.com>
2021-04-04 13:45:14 +0300

    regenerate with derives

--- src/api.rs
+++ src/api.rs
@@ -1,10 +1,34 @@
 use crate::api_params::GetUpdatesParams;
 use crate::objects::Update;
+use reqwest::Client;
+
+static BASE_API_URL: &'static str = "https://api.telegram.org/bot";
 
 pub struct API {
     api_key: String,
+    api_url: String,
 }
 
 impl API {
-    pub fn get_updates(&self, params: GetUpdatesParams) -> Update {}
+    pub fn new(api_key: String) -> Self {
+        let api_url = format!("{}{}", BASE_API_URL, api_key);
+
+        Self { api_key, api_url }
+    }
+
+    pub async fn get_updates(&self, params: GetUpdatesParams) -> Result<(), reqwest::Error> {
+        let url = format!("{}/{}", self.api_url, "getUpdates");
+
+        let result = Client::new()
+            .post(&url)
+            .json(&params)
+            .send()
+            .await?
+            .json()
+            .await?;
+
+        eprintln!("{:?}", result);
+
+        Ok(())
+    }
 }
