38775991b471c026b40dba8f21f89f49f2da1d36
Ayrat Badykov <ayratin555@gmail.com>
2021-04-11 19:11:32 +0300

    add PartialEq

--- src/api.rs
+++ src/api.rs
@@ -6,32 +6,33 @@ use crate::objects::Message;
 use crate::objects::Update;
 use isahc::{prelude::*, Request};
 use serde::{Deserialize, Serialize};
-use serde_json::Value;
 
 static BASE_API_URL: &'static str = "https://api.telegram.org/bot";
 
+#[derive(PartialEq, Debug)]
 pub struct API {
     api_url: String,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(PartialEq, Debug, Serialize, Deserialize)]
 pub struct MethodResponse<T> {
-    ok: bool,
-    result: T,
-    description: Option<String>,
+    pub ok: bool,
+    pub result: T,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub description: Option<String>,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(PartialEq, Debug, Serialize, Deserialize)]
 pub struct ErrorResponse {
-    ok: bool,
-    description: String,
-    error_code: u64,
+    pub ok: bool,
+    pub description: String,
+    pub error_code: u64,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(PartialEq, Debug, Serialize, Deserialize)]
 #[serde(untagged)]
 pub enum ApiResponse<T> {
-    Generic(ErrorResponse),
+    Error(ErrorResponse),
     ApiResult(MethodResponse<T>),
 }
 
@@ -39,6 +40,10 @@ impl API {
     pub fn new(api_key: String) -> Self {
         let api_url = format!("{}{}", BASE_API_URL, api_key);
 
+        Self { api_url }
+    }
+
+    pub fn new_url(api_url: String) -> Self {
         Self { api_url }
     }
 
@@ -83,5 +88,67 @@ impl API {
         let response: T2 = serde_json::from_reader(response.body_mut()).unwrap();
 
         Ok(response)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::api_params::*;
+    use crate::objects::*;
+    use httpmock::Method::POST;
+    use httpmock::MockServer;
+
+    #[test]
+    fn new_sets_correct_url() {
+        let api = API::new("hey".to_string());
+
+        assert_eq!(
+            API {
+                api_url: "https://api.telegram.org/bothey".to_string()
+            },
+            api
+        );
+    }
+
+    #[test]
+    fn get_updates_success() {
+        let response_string = "{\"ok\":true,\"result\":[{\"update_id\":379656753,\"message\":{\"message_id\":2741,\"from\":{\"id\":275808073,\"is_bot\":false,\"first_name\":\"Ayrat\",\"last_name\":\"Badykov\",\"username\":\"Ayrat555\",\"language_code\":\"en\"},\"date\":1618149703,\"chat\":{\"id\":275808073,\"type\":\"private\",\"username\":\"Ayrat555\",\"first_name\":\"Ayrat\",\"last_name\":\"Badykov\"},\"text\":\"dsaf\"}}]}";
+        let mut params = GetUpdatesParams::new();
+        params.set_allowed_updates(Some(vec!["message".to_string()]));
+
+        let api = api_with_mock("/getUpdates", response_string);
+
+        let response = api.get_updates(params).unwrap();
+
+        let json = serde_json::to_string(&response).unwrap();
+
+        assert_eq!(response_string, json);
+
+        match response {
+            ApiResponse::ApiResult(MethodResponse {
+                ok: true,
+                result: updates,
+                description: None,
+            }) => {
+                assert_eq!(1, updates.len());
+
+                let update = &updates[0];
+
+                assert_eq!(379656753, update.update_id())
+            }
+            _ => assert!(false),
+        }
+    }
+
+    fn api_with_mock(path: &str, response: &str) -> API {
+        let server = MockServer::start();
+
+        server.mock(|when, then| {
+            when.method(POST).path(path);
+            then.status(200).body(response);
+        });
+
+        API::new_url(server.url(""))
     }
 }
