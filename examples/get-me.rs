b371ce5edf1253121be0177ef445c2484b55c42c
Arkadiy <arkada38@outlook.com>
2021-05-19 11:54:10 +0300

    Added get me example (#9)

---
+++ examples/get-me.rs
@@ -0,0 +1,18 @@
+use frankenstein::Api;
+use frankenstein::TelegramApi;
+
+static TOKEN: &str = "API_TOKEN";
+
+fn main() {
+    let api = Api::new(TOKEN.to_string());
+
+    match api.get_me() {
+        Ok(response) => {
+            let user = response.result;
+            println!("Hello, I'm @{}, https://t.me/{}", user.first_name, user.username.expect("The bot must have a username."));
+        },
+        Err(error) => {
+            eprintln!("Failed to get me: {:?}", error);
+        }
+    }
+}
