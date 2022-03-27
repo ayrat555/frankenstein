ec12f00a6624189d851d31538fe27e4e7d67a29c
Ayrat Badykov <ayratin555@gmail.com>
2022-03-27 15:27:36 +0300

    add keyboard example

---
+++ examples/reply_keyboard.rs
@@ -0,0 +1,40 @@
+use frankenstein::Api;
+use frankenstein::KeyboardButton;
+use frankenstein::ReplyKeyboardMarkup;
+use frankenstein::ReplyMarkup;
+use frankenstein::SendMessageParams;
+use frankenstein::TelegramApi;
+
+// replace with your token
+static TOKEN: &str = "TOKEN";
+// replace with your chat id
+static CHAT_ID: i64 = 275808073;
+
+fn main() {
+    let api = Api::new(TOKEN);
+
+    let mut keyboard: Vec<Vec<KeyboardButton>> = Vec::new();
+
+    for i in 1..5 {
+        let mut row: Vec<KeyboardButton> = Vec::new();
+
+        for j in 1..5 {
+            let name = format!("{}{}", i, j);
+            let button = KeyboardButton::builder().text(name).build();
+
+            row.push(button);
+        }
+
+        keyboard.push(row);
+    }
+
+    let keyboard_markup = ReplyKeyboardMarkup::builder().keyboard(keyboard).build();
+
+    let send_message_params = SendMessageParams::builder()
+        .chat_id(CHAT_ID)
+        .text("hello!")
+        .reply_markup(ReplyMarkup::ReplyKeyboardMarkup(keyboard_markup))
+        .build();
+
+    api.send_message(&send_message_params).unwrap();
+}
