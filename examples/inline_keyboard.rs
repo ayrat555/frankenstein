86c1731196503576b4d88895836bc8db72636c6a
Ayrat Badykov <ayratin555@gmail.com>
2022-03-27 15:36:59 +0300

    add inline keyboard example

---
+++ examples/inline_keyboard.rs
@@ -0,0 +1,45 @@
+use frankenstein::Api;
+use frankenstein::InlineKeyboardButton;
+use frankenstein::InlineKeyboardMarkup;
+use frankenstein::ReplyMarkup;
+use frankenstein::SendMessageParams;
+use frankenstein::TelegramApi;
+
+// replace with your token
+static TOKEN: &str = "1276618370:AAGx5YhNQvUG4eUcQXN-OB_a09ZzYl6uh6o";
+// replace with your chat id
+static CHAT_ID: i64 = 275808073;
+
+fn main() {
+    let api = Api::new(TOKEN);
+
+    let mut keyboard: Vec<Vec<InlineKeyboardButton>> = Vec::new();
+
+    for i in 1..5 {
+        let mut row: Vec<InlineKeyboardButton> = Vec::new();
+
+        for j in 1..5 {
+            let name = format!("{}{}", i, j);
+            let button = InlineKeyboardButton::builder()
+                .text(name)
+                .url("https://github.com/ayrat555/frankenstein")
+                .build();
+
+            row.push(button);
+        }
+
+        keyboard.push(row);
+    }
+
+    let inline_keyboard = InlineKeyboardMarkup::builder()
+        .inline_keyboard(keyboard)
+        .build();
+
+    let send_message_params = SendMessageParams::builder()
+        .chat_id(CHAT_ID)
+        .text("hello!")
+        .reply_markup(ReplyMarkup::InlineKeyboardMarkup(inline_keyboard))
+        .build();
+
+    api.send_message(&send_message_params).unwrap();
+}
