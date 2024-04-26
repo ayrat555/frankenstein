892280a7c8f52110957e31135a176987f8ca9282
Ayrat Badykov <ayratin555@gmail.com>
2024-04-26 10:50:22 +0300

    fix ExternalReplyInfo decoding
    fixes https://github.com/ayrat555/frankenstein/issues/164#issuecomment-2078813442

--- src/objects.rs
+++ src/objects.rs
@@ -1014,7 +1014,9 @@ pub struct ExternalReplyInfo {
     #[builder(setter(into, strip_option), default)]
     pub chain: Option<Chat>,
 
-    pub message_id: i32,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    #[builder(setter(into, strip_option), default)]
+    pub message_id: Option<i32>,
 
     #[serde(skip_serializing_if = "Option::is_none")]
     #[builder(setter(into, strip_option), default)]
