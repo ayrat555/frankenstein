2db85b5b268b23022a331220224da507ae8f697a
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2024-08-29 13:19:06 +0200

    fix: clippy::too_long_first_doc_paragraph

--- src/api_traits.rs
+++ src/api_traits.rs
@@ -22,12 +22,14 @@ pub struct MethodResponse<T> {
     pub description: Option<String>,
 }
 
-#[derive(Debug, Serialize, Deserialize, PartialEq, Eq)]
-/// \[…\] an unsuccessful request, `ok` equals false and the error is explained in the `description`.
+/// Error on an unsuccessful request.
+///
+/// `ok` equals false and the error is explained in the `description`.
 /// An Integer `error_code` field is also returned, but its contents are subject to change in the future.
 /// Some errors may also have an optional field `parameters` of the type `ResponseParameters`, which can help to automatically handle the error.
 ///
 /// See <https://core.telegram.org/bots/api#making-requests>
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq)]
 pub struct ErrorResponse {
     /// Always false
     pub ok: bool,
