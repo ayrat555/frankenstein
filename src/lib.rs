bf4011f72a75cb83b0d79cc597cae62e3a9f7ba4
Veetaha <gersoh3@gmail.com>
2024-09-16 01:40:03 +0000

    Add a comment

--- src/lib.rs
+++ src/lib.rs
@@ -39,6 +39,10 @@ mod trait_sync;
 pub const BASE_API_URL: &str = "https://api.telegram.org/bot";
 
 macro_rules_attribute::attribute_alias! {
+    // Shared configuration for all builder derives. Make sure to add them to
+    // all API types.
+    //
+    // We enable `into` for specific types to reduce boilerplate for the callers.
     #[apply(builder!)] =
         #[derive(::bon::Builder)]
         #[builder(
